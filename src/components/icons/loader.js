fbfb4aca7eec84fdfab87ea1c7ecefff625a993e
Adhyayan Panwar <nullvoiddeath@gmail.com>
2022-02-06 14:49:08 +0530

    Changed Logo

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -10,18 +10,7 @@ const IconLoader = () => (
           fill="currentColor"
         />
       </g>
-      <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
+      
     </g>
   </svg>
 );
