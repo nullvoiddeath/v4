51ad5d05fded23e137ed0f46e40ffe1309ded2c1
Adhyayan Panwar <nullvoiddeath@gmail.com>
2022-02-06 14:55:07 +0530

    Changed Logo

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -58,8 +58,8 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const one = <h1>Hi, my name is <strike>*chicka* *chicka* slim shady</strike></h1>;
+  const two = <h2 className="big-heading">Adhyayan Panwar.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
